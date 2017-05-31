483cfb4cefc66d65b4a80baa36f2654c8efe804c
Chris Andrejewski <christopher.andrejewski@gmail.com>
2017-05-31 12:51:18 -0400

    Add event registry and tests

--- can-dom-events-test.js
+++ can-dom-events-test.js
@@ -1,9 +1,84 @@
-// There were no tests for domEvents directly
 var unit = require('steal-qunit');
 var domEvents = require('./can-dom-events');
 
 unit.module('can-dom-events');
 
-unit.test('can-dom-events should exist', function (assert) {
-    assert.ok(domEvents);
+unit.test('domEvents.addEventListener works', function (assert) {
+    // We check that our handler gets called if we add it through domEvents.
+    // NOTE: the event is not triggered via domEvents.dispatch to isolate this test.
+    assert.expect(1);
+
+    var input = document.createElement('input');
+    var eventType = 'click';
+    var handler = function () {
+        assert.ok(true, 'event handler should be called');
+    };
+
+    domEvents.addEventListener(input, eventType, handler);
+
+    var event = new Event(eventType);
+    input.dispatchEvent(event);
+
+    domEvents.removeEventListener(input, eventType, handler);
 });
+
+unit.test('domEvents.removeEventListener works', function (assert) {
+    // We check that our handler gets called if we add/remove it through domEvents.
+    // NOTE: the event is not triggered via domEvents.dispatch to isolate this test.
+    assert.expect(1);
+
+    var input = document.createElement('input');
+    var eventType = 'click';
+    var handler = function () {
+        assert.ok(true, 'event handler should be called');
+    };
+
+    domEvents.addEventListener(input, eventType, handler);
+
+    var event = new Event(eventType);
+    input.dispatchEvent(event);
+
+    domEvents.removeEventListener(input, eventType, handler);
+
+    var event2 = new Event(eventType);
+    input.dispatchEvent(event2);
+});
+
+unit.test('domEvents.canAddEventListener works', function (assert) {
+    // Our definition of what can be addEventListener-ed to
+    // is more strict than EventTarget. 
+    // It must be a DOM Node, Document, or window.
+    var element = document.createElement('div');
+    assert.equal(domEvents.canAddEventListener(element), true, 'Elements work');
+
+    assert.equal(domEvents.canAddEventListener(document), true, 'Documents work');
+
+    assert.equal(domEvents.canAddEventListener(window), true, 'Window works');
+
+    assert.equal(domEvents.canAddEventListener(8), false, 'Numbers should not work');
+    assert.equal(domEvents.canAddEventListener("foo"), false, 'Strings should not work');
+    assert.equal(domEvents.canAddEventListener({a: 1}), false, 'Plain objects should not work');
+
+    var textNode = document.createTextNode('boi');
+    assert.equal(domEvents.canAddEventListener(textNode), false, 'Text nodes should not work');
+});
+
+unit.test('domEvents.dispatch works', function (assert) {
+    // NOTE: dispatching should work no matter how a listener was add.
+    // So we use the native addEventListener to isolate the dispatch.
+    assert.expect(1);
+
+    var input = document.createElement('input');
+    var eventType = 'click';
+    var handler = function () {
+        assert.ok(true, 'event handler should be called');
+    };
+
+    input.addEventListener(eventType, handler);
+
+    domEvents.dispatch(input, eventType);
+
+    input.removeEventListener(eventType, handler);
+});
+
+require('./helpers/make-event-registry-test');
--- can-dom-events.js
+++ can-dom-events.js
@@ -1,16 +1,9 @@
 'use strict';
 
 var namespace = require('can-namespace');
-var _document = require("can-util/dom/document/document");
-var isBrowserWindow = require("can-util/js/is-browser-window/is-browser-window");
-var isPlainObject = require("can-util/js/is-plain-object/is-plain-object");
-var fixSyntheticEventsOnDisabled = false;
+var util = require('./helpers/util');
+var makeEventRegistry = require('./helpers/make-event-registry');
 
-function isDispatchingOnDisabled(element, ev) {
-	var isInsertedOrRemoved = isPlainObject(ev) ? (ev.type === 'inserted' || ev.type === 'removed') : (ev === 'inserted' || ev === 'removed');
-	var isDisabled = !!element.disabled;
-	return isInsertedOrRemoved && isDisabled;
-}
 /**
  * @module {{}} can-dom-events
  * @parent can-infrastructure
@@ -20,70 +13,67 @@ function isDispatchingOnDisabled(element, ev) {
  * var domEvents = require("can-dom-events");
  * ```
  */
-module.exports = namespace.domEvents = {
-	addEventListener: function(){
-		this.addEventListener.apply(this, arguments);
-	},
-	removeEventListener: function(){
-		this.removeEventListener.apply(this, arguments);
-	},
-	canAddEventListener: function(){
-		return (this.nodeName && (this.nodeType === 1 || this.nodeType === 9)) || this === window;
-	},
-	dispatch: function(event, args, bubbles){
-		var ret;
-		var dispatchingOnDisabled = fixSyntheticEventsOnDisabled && isDispatchingOnDisabled(this, event);
+var domEvents = {
+    _eventRegistry: makeEventRegistry(),
 
-		var doc = this.ownerDocument || _document();
-		var ev = doc.createEvent('HTMLEvents');
-		var isString = typeof event === "string";
+    addEvent (event, eventType) {
+        return this._eventRegistry.add(event, eventType);
+    },
 
-		// removed / inserted events should not bubble
-		ev.initEvent(isString ? event : event.type, bubbles === undefined ? true : bubbles, false);
+	addEventListener: function() {
+        var args = util.addDomContext(this, arguments);
+        var eventType = args[1];
+        var hasCustomEvent = domEvents._eventRegistry.has(eventType);
+        if (hasCustomEvent) {
+            var eventDefinition = domEvents._eventRegistry.get(eventType);
+            return eventDefinition.addEventListener.apply(domEvents, args);
+        }
 
-		if(!isString) {
-			for (var prop in event) {
-				if (ev[prop] === undefined) {
-					ev[prop] = event[prop];
-				}
-			}
+        var target = args[0];
+		var eventArgs = Array.prototype.slice.call(args, 1);
+        return target.addEventListener.apply(target, eventArgs);
+	},
+    
+	removeEventListener: function() {
+        var args = util.addDomContext(this, arguments);
+        var eventType = args[1];
+        var hasCustomEvent = domEvents._eventRegistry.has(eventType);
+        if (hasCustomEvent) {
+            var eventDefinition = domEvents._eventRegistry.get(eventType);
+            return eventDefinition.removeEventListener.apply(domEvents, args);
+        }
+
+		var target = args[0];
+		var eventArgs = Array.prototype.slice.call(args, 1);
+        return target.removeEventListener.apply(target, eventArgs);
+	},
+
+	canAddEventListener: function() {
+        var target = util.addDomContext(this, arguments)[0];
+		return util.isDomEventTarget(target);
+	},
+
+	dispatch: function() {
+        var args = util.addDomContext(this, arguments);
+        var target = args[0];
+        var eventData = args[1];
+        var eventArgs = args[2];
+        var bubbles = args[3];
+        var cancelable = false;
+
+		var event = util.createEvent(this, eventData, eventArgs, bubbles, cancelable);
+		var enableForDispatch = util.forceEnabledForDispatch(target, event);
+		if(enableForDispatch) {
+			target.disabled = false;
 		}
-		ev.args = args;
-		if(dispatchingOnDisabled) {
-			this.disabled = false;
+
+		var ret = target.dispatchEvent(event);
+		if(enableForDispatch) {
+			target.disabled = true;
 		}
-		ret = this.dispatchEvent(ev);
-		if(dispatchingOnDisabled) {
-			this.disabled = true;
-		}
+
 		return ret;
 	}
 };
 
-// In FireFox, dispatching a synthetic event on a disabled element throws an error.
-// Other browsers, like IE 10 do not dispatch synthetic events on disabled elements at all.
-// This determines if we have to work around that when dispatching events.
-// https://bugzilla.mozilla.org/show_bug.cgi?id=329509
-(function() {
-	if(!isBrowserWindow()) {
-		return;
-	}
-
-	var testEventName = 'fix_synthetic_events_on_disabled_test';
-	var input = document.createElement("input");
-	input.disabled = true;
-	var timer = setTimeout(function() {
-		fixSyntheticEventsOnDisabled = true;
-	}, 50);
-	var onTest = function onTest (){
-		clearTimeout(timer);
-		module.exports.removeEventListener.call(input, testEventName, onTest);
-	};
-	module.exports.addEventListener.call(input, testEventName, onTest);
-	try {
-		module.exports.dispatch.call(input, testEventName, [], false);
-	} catch(e) {
-		onTest();
-		fixSyntheticEventsOnDisabled = true;
-	}
-})();
+module.exports = namespace.domEvents = domEvents;
