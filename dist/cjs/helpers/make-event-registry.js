9e4134aa449b957f8739a9683a0cc57484da5416
Chris Andrejewski <christopher.andrejewski@gmail.com>
2017-06-13 14:51:14 -0400

    0.0.1

---
+++ dist/cjs/helpers/make-event-registry.js
@@ -0,0 +1,40 @@
+/*can-dom-events@0.0.0#helpers/make-event-registry*/
+'use strict';
+function EventRegistry() {
+    this._registry = {};
+}
+module.exports = function makeEventRegistry() {
+    return new EventRegistry();
+};
+EventRegistry.prototype.has = function (eventType) {
+    return !!this._registry[eventType];
+};
+EventRegistry.prototype.get = function (eventType) {
+    return this._registry[eventType];
+};
+EventRegistry.prototype.add = function (event, eventType) {
+    if (arguments.length === 1) {
+        return this.add(event, event.defaultEventType);
+    }
+    if (typeof eventType !== 'string') {
+        throw new TypeError('Event type must be a string, not ' + eventType);
+    }
+    if (!event) {
+        throw new Error('An event must be provided');
+    }
+    if (typeof event.addEventListener !== 'function') {
+        throw new TypeError('Event addEventListener must be a function');
+    }
+    if (typeof event.removeEventListener !== 'function') {
+        throw new TypeError('Event removeEventListener must be a function');
+    }
+    var existingEvent = this._registry[eventType];
+    if (existingEvent) {
+        throw new Error('Event "' + eventType + '" is already registered');
+    }
+    this._registry[eventType] = event;
+    var self = this;
+    return function remove() {
+        self._registry[eventType] = undefined;
+    };
+};
