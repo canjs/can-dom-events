3783930ea5aa068046896f393969b7020602e9f8
Chris Andrejewski <christopher.andrejewski@gmail.com>
2017-06-13 15:45:26 -0400

    0.0.3

---
+++ dist/amd/dist/cjs/can-dom-events.js
@@ -0,0 +1,44 @@
+/*can-dom-events@0.0.1#dist/cjs/can-dom-events*/
+define(function (require, exports, module) {
+    'use strict';
+    var namespace = require('can-namespace');
+    var util = require('./helpers/util');
+    var makeEventRegistry = require('./helpers/make-event-registry');
+    var domEvents = {
+        _eventRegistry: makeEventRegistry(),
+        addEvent: function (event, eventType) {
+            return this._eventRegistry.add(event, eventType);
+        },
+        addEventListener: function (target, eventType) {
+            var hasCustomEvent = domEvents._eventRegistry.has(eventType);
+            if (hasCustomEvent) {
+                var event = domEvents._eventRegistry.get(eventType);
+                return event.addEventListener.apply(domEvents, arguments);
+            }
+            var eventArgs = Array.prototype.slice.call(arguments, 1);
+            return target.addEventListener.apply(target, eventArgs);
+        },
+        removeEventListener: function (target, eventType) {
+            var hasCustomEvent = domEvents._eventRegistry.has(eventType);
+            if (hasCustomEvent) {
+                var event = domEvents._eventRegistry.get(eventType);
+                return event.removeEventListener.apply(domEvents, arguments);
+            }
+            var eventArgs = Array.prototype.slice.call(arguments, 1);
+            return target.removeEventListener.apply(target, eventArgs);
+        },
+        dispatch: function (target, eventData, bubbles, cancelable) {
+            var event = util.createEvent(target, eventData, bubbles, cancelable);
+            var enableForDispatch = util.forceEnabledForDispatch(target, event);
+            if (enableForDispatch) {
+                target.disabled = false;
+            }
+            var ret = target.dispatchEvent(event);
+            if (enableForDispatch) {
+                target.disabled = true;
+            }
+            return ret;
+        }
+    };
+    module.exports = namespace.domEvents = domEvents;
+});
---
+++ dist/amd/helpers/util.js
@@ -0,0 +1,95 @@
+/*can-dom-events@0.0.2#helpers/util*/
+define(function (require, exports, module) {
+    'use strict';
+    var getCurrentDocument = require('can-util/dom/document');
+    var isBrowserWindow = require('can-util/js/is-browser-window');
+    function getTargetDocument(target) {
+        return target.ownerDocument || getCurrentDocument();
+    }
+    function createEvent(target, eventData, bubbles, cancelable) {
+        var doc = getTargetDocument(target);
+        var event = doc.createEvent('HTMLEvents');
+        var eventType;
+        if (typeof eventData === 'string') {
+            eventType = eventData;
+        } else {
+            eventType = event.type;
+            for (var prop in eventData) {
+                if (event[prop] === undefined) {
+                    event[prop] = event[prop];
+                }
+            }
+        }
+        if (bubbles === undefined) {
+            bubbles = true;
+        }
+        event.initEvent(eventType, bubbles, cancelable);
+        return event;
+    }
+    function isDomEventTarget(obj) {
+        if (!(obj && obj.nodeName)) {
+            return obj === window;
+        }
+        var nodeType = obj.nodeType;
+        return nodeType === Node.DOCUMENT_NODE || nodeType === Node.ELEMENT_NODE;
+    }
+    function addDomContext(context, args) {
+        if (isDomEventTarget(context)) {
+            args = Array.prototype.slice.call(args, 0);
+            args.unshift(context);
+        }
+        return args;
+    }
+    function removeDomContext(context, args) {
+        if (!isDomEventTarget(context)) {
+            args = Array.prototype.slice.call(args, 0);
+            context = args.shift();
+        }
+        return {
+            context: context,
+            args: args
+        };
+    }
+    var fixSyntheticEventsOnDisabled = false;
+    (function () {
+        if (!isBrowserWindow()) {
+            return;
+        }
+        var testEventName = 'fix_synthetic_events_on_disabled_test';
+        var input = document.createElement('input');
+        input.disabled = true;
+        var timer = setTimeout(function () {
+            fixSyntheticEventsOnDisabled = true;
+        }, 50);
+        var onTest = function onTest() {
+            clearTimeout(timer);
+            input.removeEventListener(testEventName, onTest);
+        };
+        input.addEventListener(testEventName, onTest);
+        try {
+            var event = document.create('HTMLEvents');
+            event.initEvent(testEventName, false);
+            input.dispatchEvent(event);
+        } catch (e) {
+            onTest();
+            fixSyntheticEventsOnDisabled = true;
+        }
+    }());
+    function isDispatchingOnDisabled(element, event) {
+        var eventType = event.type;
+        var isInsertedOrRemoved = eventType === 'inserted' || eventType === 'removed';
+        var isDisabled = !!element.disabled;
+        return isInsertedOrRemoved && isDisabled;
+    }
+    function forceEnabledForDispatch(element, event) {
+        return fixSyntheticEventsOnDisabled && isDispatchingOnDisabled(element, event);
+    }
+    module.exports = {
+        createEvent: createEvent,
+        addDomContext: addDomContext,
+        removeDomContext: removeDomContext,
+        isDomEventTarget: isDomEventTarget,
+        getTargetDocument: getTargetDocument,
+        forceEnabledForDispatch: forceEnabledForDispatch
+    };
+});
